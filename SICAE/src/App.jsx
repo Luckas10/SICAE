d34ef4c902f246a56e4977f0fcf067d7209f2673
André Medeiros Silva <andre1998vv@gmail.com>
2025-10-13 21:38:12 -0300

    React Install

---
+++ SICAE/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from './pages/Login.jsx';
+import Register from './pages/Register.jsx';
+
+function App() {
+
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
---
+++ SICAE/src/pages/Login.jsx
@@ -0,0 +1,29 @@
+import './auth.css';
+
+export default function Login() {
+
+    return (
+        <>
+        <head>
+            <meta charset="UTF-8"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <title>SICAE - Login</title>
+        </head>
+            <div class="main">
+            <div class="auth login">
+                <form action="">
+                <img class="avatar" src="../assets/user-icon.png" alt="Usuário"/>
+                <input type="text" placeholder="Usuário"/>
+                <input type="password" placeholder="Senha"/>
+                <a href="/register" class="form-link">Ainda não tem uma conta?</a>
+                <button type="submit">Login</button>
+                </form>
+            </div>
+
+            <div class="logo">
+                <div class="img"></div>
+            </div>
+            </div>
+        </>
+    );
+}

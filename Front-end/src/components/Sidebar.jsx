4d43582cd465fcaaebe7aeca42063a4b74e9f056
JulioTVRS <juliotvrs777@gmail.com>
2025-10-25 15:29:23 -0300

    Fixed sidebar: SVG color and responsiveness

--- Front-end/src/components/Sidebar.jsx
+++ Front-end/src/components/Sidebar.jsx
@@ -1,25 +1,25 @@
 import './Sidebar.css'
-import Home from '../assets/icons/home-icon.svg'
-import Events from '../assets/icons/events-icon.svg'
-import Noticias from '../assets/icons/news-icon.svg'
-import Usuarios from '../assets/icons/users-icon.svg'
-import Configuracoes from '../assets/icons/config-icon.svg'
-import Logout from '../assets/icons/logout-icon.svg'
+import Home from '../assets/icons/home-icon.svg?react'
+import Events from '../assets/icons/events-icon.svg?react'
+import Noticias from '../assets/icons/news-icon.svg?react'
+import Usuarios from '../assets/icons/users-icon.svg?react'
+import Configuracoes from '../assets/icons/config-icon.svg?react'
+import Logout from '../assets/icons/logout-icon.svg?react'
 
 
 export default function Sidebar() {
   return (
     <aside className="sidebar">
       <div className="links">
-        <button className="side-link"><a href="#"><img className="img" src={Home} alt="" /><span>Página Inicial</span></a></button>
-        <button className="side-link"><a href="#"><img className="img" src={Events} alt="" /><span>Eventos</span></a></button>
-        <button className="side-link"><a href="#"><img className="img" src={Noticias} alt="" /><span>Notícias</span></a></button>
-        <button className="side-link"><a href="#"><img className="img" src={Usuarios} alt="" /><span>Usuários</span></a></button>
-        <button className="side-link"><a href="#"><img className="img" src={Configuracoes} alt="" /><span>Configurações</span></a></button>
+        <button className="side-link"><a href="#"><Home className="img"/><span>Página Inicial</span></a></button>
+        <button className="side-link"><a href="#"><Events className="img"/><span>Eventos</span></a></button>
+        <button className="side-link"><a href="#"><Noticias className="img"/><span>Notícias</span></a></button>
+        <button className="side-link"><a href="#"><Usuarios className="img"/><span>Usuários</span></a></button>
+        <button className="side-link"><a href="#"><Configuracoes className="img"/><span>Configurações</span></a></button>
       </div>
 
       <div className="logout">
-        <button className="side-link"><a href="#"><img className="img" src={Logout} alt="" /><span>Sair</span></a></button>
+        <button className="side-link"><a href="#"><Logout className="img"/><span>Sair</span></a></button>
       </div>
     </aside>
   )

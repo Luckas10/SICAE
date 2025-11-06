a06895f7dacff6675a5f886291441e9cdccce9b7
Andremdrs77 <andre1998vv@gmail.com>
2025-11-06 16:03:21 -0300

    Small advances

--- Front-end/src/pages/Dashboard.jsx
+++ Front-end/src/pages/Dashboard.jsx
@@ -15,43 +15,48 @@ export default function Dashboard() {
             <div className='dashboard-page'>
                 <Sidebar />
                 <div className='dashboard-content'>
-                    <Carousel>
-                        <Carousel.Item>
-                            <img
-                                className="d-block"
-                                src={img1}
-                                alt="Primeiro slide"
-                            />
-                            <Carousel.Caption>
-                                <h3>Primeiro slide label</h3>
-                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                            </Carousel.Caption>
-                        </Carousel.Item>
+                    <div className="main-content">
+                        <Carousel>
+                            <Carousel.Item>
+                                <img
+                                    className="d-block"
+                                    src={img1}
+                                    alt="Primeiro slide"
+                                />
+                                <Carousel.Caption>
+                                    <h3>Primeiro slide label</h3>
+                                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
 
-                        <Carousel.Item>
-                            <img
-                                className="d-block"
-                                src={img2}
-                                alt="Segundo slide"
-                            />
-                            <Carousel.Caption>
-                                <h3>Segundo slide label</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            </Carousel.Caption>
-                        </Carousel.Item>
+                            <Carousel.Item>
+                                <img
+                                    className="d-block"
+                                    src={img2}
+                                    alt="Segundo slide"
+                                />
+                                <Carousel.Caption>
+                                    <h3>Segundo slide label</h3>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
 
-                        <Carousel.Item>
-                            <img
-                                className="d-block"
-                                src={img1}
-                                alt="Terceiro slide"
-                            />
-                            <Carousel.Caption>
-                                <h3>Terceiro slide label</h3>
-                                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                            </Carousel.Caption>
-                        </Carousel.Item>
-                    </Carousel>
+                            <Carousel.Item>
+                                <img
+                                    className="d-block"
+                                    src={img1}
+                                    alt="Terceiro slide"
+                                />
+                                <Carousel.Caption>
+                                    <h3>Terceiro slide label</h3>
+                                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                        </Carousel>
+                        <div className="notifications">
+                            <p>oiii</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>

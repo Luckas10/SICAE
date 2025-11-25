1e23ad391b167afa49b9428b49c9e2d6004551ef
eunicecristina <eunicecristina0510@gmail.com>
2025-11-25 09:25:57 -0300

    link

--- Front-end/src/components/general/Header.jsx
+++ Front-end/src/components/general/Header.jsx
@@ -1,5 +1,6 @@
 import { useUser } from "../../context/UserContext";
 import headerLogo from '../../assets/Logotipo-SICAE.png';
+import { NavLink, useNavigate } from "react-router-dom";
 import './Header.css';
 
 const DEFAULT_AVATAR =
@@ -17,10 +18,12 @@ export default function Header() {
 
             <div className="user-info">
                 <p>{loadingUser ? "Carregando..." : user?.full_name}</p>
-                <img
+                <NavLink to="/profile">
+                    <img
                     src={user?.profile_image || DEFAULT_AVATAR}
                     alt="Foto de perfil"
                 />
+                </NavLink>
             </div>
         </header>
     );

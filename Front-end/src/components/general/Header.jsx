b6371cab297d1ce0aeef8cd2c78d4bb01da0d71b
André Medeiros Silva <andre1998vv@gmail.com>
2025-11-26 00:09:48 -0300

    User info: username now appears on header

--- Front-end/src/components/general/Header.jsx
+++ Front-end/src/components/general/Header.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
 import headerLogo from '../../assets/Logotipo-SICAE.png';
 import './Header.css';
+import api from '../../services/api.js';
 
 export default function Header() {
+
+    const [username, setUsername] = useState("");
+
+    useEffect(() => {
+        async function fetchUser() {
+            try {
+                const response = await api.get('/users/me');
+                setUsername(response.data.full_name);
+            } catch (error) {
+                console.error("Erro ao buscar dados do usuário:", error);
+            }
+        }
+
+        fetchUser();
+    }, []);
 
     return (
         <header className="header">
@@ -9,7 +26,7 @@ export default function Header() {
                 <img src={headerLogo} alt="SICAE" />
             </div>
             <div className="user-info">
-                <p>Nome de Usuário da Silva Gomes</p>
+                <p>{username || "Carregando..."}</p>
                 <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="" />
             </div>
         </header>

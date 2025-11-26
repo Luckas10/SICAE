023f975a879611a328e0b895b73a4cbd7a479a65
Eunice Cristina de Araújo Silva <20221101110019@ifrn.local>
2025-11-26 14:07:47 -0300

    user

--- Front-end/src/pages/Profile.jsx
+++ Front-end/src/pages/Profile.jsx
@@ -1,6 +1,7 @@
 import './Profile.css';
 import Header from '../components/Header.jsx'
 import Sidebar from '../components/Sidebar.jsx';
+import news2 from '../assets/news2.jpeg';
 
 export default function Profile() {
 
@@ -10,7 +11,30 @@ export default function Profile() {
             <div className='profile-page'>
                 <Sidebar />
                 <div className='profile-content'>
-                    <p>Conteúdo</p>
+                    <div className="profile-info">
+                        <div>
+                            <img src={news2} id='image' />
+                        <b><p>Nome do usuário</p></b>
+                        </div>
+                        <div>
+                            <div className="card">
+                                <p><b>Email:</b>eunicecristina0510@gmail.com</p>
+                            </div>
+                            <div className="card">
+                                <p><b>Matrícula:</b>20221101110019</p>
+                            </div>
+                            <div className="card">
+                                <p><b>Comentarios feitos</b>777</p>
+                            </div>
+                            <div className="card">
+                                <p><b>Eventos participados:</b>777</p>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="profile-games">
+
+                    </div>
                 </div>
             </div>
         </>

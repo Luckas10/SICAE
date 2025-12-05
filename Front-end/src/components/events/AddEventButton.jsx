ef2fbc14a1dc483e0acd9fe2af2952eb94be8fc7
André Medeiros Silva <andre1998vv@gmail.com>
2025-12-05 16:47:55 -0300

    First dark mode implementation

--- Front-end/src/components/events/AddEventButton.jsx
+++ Front-end/src/components/events/AddEventButton.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 export default function AddEventButton() {
     return (
         <NavLink to="/events/add" id="buttonAdd">
-            <button type="button">+ Adicionar evento</button>
+            <button type="button" className="buttonAdd">+ Adicionar evento</button>
         </NavLink>
     );
 }
